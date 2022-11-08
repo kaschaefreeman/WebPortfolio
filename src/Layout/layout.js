4e310086a23e3dc9f783fcf5be65f9c747b4233c
Kaschae Freeman <kaschaefreeman@gmail.com>
2022-11-07 22:20:39 -0500

    Update navigation bar: 1. Changed navbar color 2. Added navbar brand 3. Changed the link colors
    Update Header:
    1. Changed layout to be larger image with pentagon cut shape
    2. Moved header text from first column to second column and removed name since it is in the navbar
    3. Added links for contact information and resume instead of in footer
    
    Removed footer

--- src/Layout/layout.js
+++ src/Layout/layout.js
@@ -3,22 +3,20 @@ import AboutMe from "../sections/aboutMe";
 import Highlights from "../sections/highlights";
 import Projects from "../sections/projects";
 import TechnicalSkills from "../sections/technicalSkills";
-import Footer from "./footer";
-import Header from "./Header";
-import Navigation from "./navigation";
+import Header from "./header/Header";
+import Navigation from "./navigation/navigation";
 
 const Layout = () => {
   return (
-    <main className="bg-light">
-      <Header />
+    <main >
       <Navigation />
+      <Header />
       <article className="container">
         <AboutMe />
         <Highlights />
         <TechnicalSkills />
         <Projects />
       </article>
-      <Footer />
     </main>
   );
 };
