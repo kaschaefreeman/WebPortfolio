69b7afed76bfd92558fa62d9c44cc352519a4506
Kaschae Freeman <kaschaefreeman@gmail.com>
2023-02-14 21:39:21 -0500

    remove animation from each heading seperately.  change animation to be together

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,13 +6,13 @@ const Header = () => {
 
   return (
     <header>
-      <div className="headerContainer">
-        <h2 className="titleRow" data-aos="fade-left">
+      <div className="headerContainer" data-aos="fade-left">
+        <h2 className="titleRow">
           <span className="col-auto">Software Developer</span>
           <span className="col-2 mx-3 vr headingSeparator"></span>
           <span className="col-auto">Woodbridge, VA</span>
         </h2>
-        <p className="lead col-12" data-aos="fade-left">
+        <p className="lead col-12">
           Motivated junior software engineer proficient in JavaScript for
           front-end and back-end programming.
         </p>
